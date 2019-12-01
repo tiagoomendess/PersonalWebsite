41b083eefb83c0a8daa59e034b19dc008ae98681
tiagoomendess <tiago@mendes.com.pt>
2019-12-01 00:08:19 +0000

    Deploy script improved

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,33 @@
-echo "Starting deploy!"
+echo "#########################"
+echo "#   Starting deploy!"   #
+echo "#########################"
 
+echo "--- Taking application down ------------"
 php artisan down
+
 git reset --hard
 git pull
 php artisan cache:clear
+
+echo "--- Composer install -----------"
 composer install
+
+echo "--- NPM nnstall ------------"
+npm install
+
+echo "--- Compiling sccs and js ------------"
 npm run production
+
+echo "--- Running migrations ------------"
 php artisan migrate
+
+echo "--- Dealing with permissions ------------"
 chmod 755 -R ../meusite/
 chown www-data ../meusite/ -R
+
+echo "--- Application Up! ------------"
 php artisan up
 
-echo "Finished!"
+echo "#################"
+echo "#   Finished!   #"
+echo "#################"
