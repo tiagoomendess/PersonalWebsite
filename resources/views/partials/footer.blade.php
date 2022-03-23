689921c64137c129780e5b57014b3bacdbcef7a8
Tiago Mendes <tiagoomendess@gmail.com>
2022-03-23 16:00:46 +0000

    fixed height

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
         <a class="locale-flags" href="{{ route('set_locale', ['locale' => $locale]) }}">
             <img
                     class="{{ $locale == \Illuminate\Support\Facades\App::getLocale() ? 'locale-flag-selected' : 'locale-not-flag-selected' }}"
-                    src="https://flagcdn.com/h40/{{ \App\Http\Controllers\LocaleController::LOCALE_TO_COUNTRY_CODE[$locale] }}.png">
+                    src="https://flagcdn.com/h20/{{ \App\Http\Controllers\LocaleController::LOCALE_TO_COUNTRY_CODE[$locale] }}.png">
         </a>
     @endforeach
 </div>
